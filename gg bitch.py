9380dc82c5e2791ec31042eafc1bf35b0f662fe0
Tka4enk0UA <93079121+Tka4enk0UA@users.noreply.github.com>
2021-10-24 14:41:35 +0300

    Add files via upload

---
+++ gg bitch.py
@@ -0,0 +1,12 @@
+from PyQt5.Qtwidgets import *
+
+button_menu - QPushButton("Menu")
+slepp = QPushButton("Relax")
+box_minutes = QSpinBox()
+box_minutes_setValue(30)
+text = Qlabel('Minutes')
+question = Qlabel('When born philip II ?')
+ans1 = QRadioButton()
+ans2 = QRadioButton()
+ans3 = QRadioButton()
+ans4 = QRadioButton()
